647cb6eb3e3cd35de212bdaf2022891231b2da74
AHAOAHA <ahaoaha_@outlook.com>
2019-07-21 15:21:03 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+##################################################
+# Copyright(C) 2016-2019. All right reserved.
+# 
+# Filename: install.sh
+# Author: ahaoozhang
+# Date: 2019-07-21 15:19:27 (星期日)
+# Describe: 
+##################################################
 cp -rf ./plugin/comment-info ~/.vim/plugin/
 #rm -rf ./.git
 echo "install ok! :)"
